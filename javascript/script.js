5903123b5e8b8a18ba3b919199d2dd568a82b88d
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-08-16 17:30:51 -0300

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,13 @@
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.body;
-const contatoImg = document.getElementById("contato-img");
 
 darkModeToggle.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
     darkModeToggle.classList.toggle("dark");
 
-    // Verifica se está no modo escuro ou claro e altera a imagem
     if (body.classList.contains("dark-mode")) {
-        contatoImg.src = "images/StudioC_WhatsAppQR_fbranco.png"; // Substitua pelo nome da sua imagem com fundo branco
         darkModeToggle.textContent = "🌚"; // Altera o texto para "Modo Claro"
     } else {
-        contatoImg.src = "images/StudioC_WhatsAppQR.png"; // Substitua pelo nome da sua imagem original com fundo transparente
         darkModeToggle.textContent = "🌞"; // Altera o texto de volta para "Modo Escuro"
     }
 });
